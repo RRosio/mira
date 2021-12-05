ffbdf1ac7b985369a784dd8d03fa211374c23136
RRosio <rosio_reyes@yahoo.com>
2021-12-05 04:23:56 -0800

    diverging ideas between search-bar and a select component. Switching to autocomple to best meet needs

--- client/src/components/CoinSearchBar.tsx
+++ client/src/components/CoinSearchBar.tsx
@@ -1,10 +1,36 @@
-import { Grid } from '@mui/material';
+import { Grid, Select, MenuItem, FormControl, InputLabel, Autocomplete } from '@mui/material';
 import SearchBar from "material-ui-search-bar";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { FC } from 'react';
 import '../styles/CoinSearchBar.css';
 
-const CoinSearchBar = () => {
+const CoinSearchBar : FC<{currencies:string[]}> = ({currencies}) => {
     const [value, setValueState] = useState("");
+    const [items, setItemsState] = useState([<MenuItem></MenuItem>]);
+    console.log('currencies from coin search bar: ', currencies);
+    useEffect(() => {
+        const results = currencies.filter(currency => {
+            return currency.toUpperCase().includes(value);
+        });
+        console.log('search results: ', results);
+        const items = generateMenuItems(results);
+        console.log('items ', items);
+        setItemsState(items);
+    }, [value, currencies]);
+
+    const generateMenuItems = (results: string[]) => {
+        const conditional : Array<JSX.Element> = [];
+
+        for(let i = 0; i < results.length; i++){
+            conditional.push(
+                <MenuItem key={i} value={results[i]}>
+                    {results[i]}
+                </MenuItem>
+            );
+        }
+        return conditional;
+    };
+        
     return (
         <>
             <Grid className="SearchContainer" container>
@@ -14,6 +40,12 @@ const CoinSearchBar = () => {
                     onChange={(newValue) => setValueState(newValue)}/>
                 </Grid>
             </Grid>
+            <FormControl>
+                <InputLabel>Coin Id</InputLabel>
+                <Select>
+                    {items}
+                </Select>
+            </FormControl>
         </>
     );
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,9 +4,35 @@ import logo from '../icons/logo.svg';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
 import CoinSearchBar from './CoinSearchBar';
+import { useState, useEffect } from 'react';
 
 const Navbar: FC = () => {
+    const [currenciesNames, setCurrenciesNames] = useState([""]);
+    const [currenciesIds, setCurrenciesIds] = useState([""]);
+    const getListOfCurrencies = () => {
+        fetch('/coinbase/currencies')
+        .then(response => response.json())
+        .then(data => {
+            let currencyNames = [];
+            let currencyIds = [];
+            const currencies = data[Object.keys(data)[0]];
+            console.log('currencies: ', currencies);
+            
+            for(let i = 0; i < currencies.length; i++){
+                currencyNames.push(currencies[i].name);
+                currencyIds.push(currencies[i].id);
+            }
+            setCurrenciesNames(currencyNames);
+            setCurrenciesIds(currencyIds);
+        }).catch(err => {
+            console.log('error: ', err);
+        });
+    }
     
+    useEffect(() => {
+        getListOfCurrencies();
+    }, []);
+
     return (
         <nav>
             <Grid container>
@@ -16,7 +42,7 @@ const Navbar: FC = () => {
                     </a> 
                 </Grid>
                 <Grid className="findButton" item xs={6}>
-                    <CoinSearchBar/>
+                    <CoinSearchBar currencies={currenciesIds}/>
                 </Grid>
                 <Grid className="favoritesButton" item xs={2}>
                     <Button>View Favorites</Button>
